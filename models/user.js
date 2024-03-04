e5631e2fc8cd326c4e73faec4a242a8014a4c8ab
Ziad Abd El Wareth <Ziad.Wareth@gmail.com>
2024-03-05 00:08:59 +0200

    adding index.js

---
+++ models/user.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    userId: {
+        type: Number,
+        required: true,
+        unique: true,
+      },
+    userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: false,
+  },
+  emailAddress: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+  },
+  isConfirmed: {
+    type: Number,
+    required: true,
+  },
+  // tickets: {
+  //   type: [Ticket],
+  //   required: false,
+  // },
+});
+
+module.exports = mongoose.model("User", userSchema);
