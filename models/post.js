30c7ba44f77a7e69d44f000fdb3facdbdfa65f14
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-03-13 22:39:57 +0200

    Update post.js

---
+++ models/post.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const { boolean } = require("webidl-conversions");
+
+const Schema = mongoose.Schema;
+const postSchema = new Schema({
+  post_ID: {
+    type: Number,
+    required: true,
+    unique: true,
+    primary: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  votes: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  isNSFW: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  isSpoiler: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  isLocked: {
+    type: Boolean,
+    required: true,
+    default: false
+    
+  }
+});
+
+module.exports = mongoose.model("post", postSchema);
