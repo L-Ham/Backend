2b1b2eaa0f49b5fd94b19cde2eb7a3dbbd9de873
Ziad Abd El Wareth <Ziad.Wareth@gmail.com>
2024-03-13 23:30:33 +0200

    Update report.js

--- models/report.js
+++ models/report.js
@@ -1,32 +1,32 @@
-const moon = require('mongoose');
-const Schema = moon.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const reportSchema = new Schema({
-    reportId: {
-        type: Number,
-        required: true
-    },
-    referenceType: {
-        type: String,
-        enum: ['user', 'post', 'comment'],
-        required: true
-    },
-    referenceId: {
-        type: Number,
-        required: true
-    },
-    viewerId: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    isReviewed: {
-        type: Number,
-        required: true
-    }
+  reportId: {
+    type: Number,
+    required: true,
+  },
+  referenceType: {
+    type: String,
+    enum: ["user", "post", "comment"],
+    required: true,
+  },
+  referenceId: {
+    type: Number,
+    required: true,
+  },
+  viewerId: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  isReviewed: {
+    type: Number,
+    required: true,
+  },
 });
 
-module.exports = moon.model('report', reportSchema);
+module.exports = moon.model("report", reportSchema);
