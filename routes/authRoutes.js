127bd77a4e668eafc93734258ee3596d3e12e75b
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-03-14 01:24:58 +0200

    forgot username

---
+++ routes/authRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+const bodyParser = require("body-parser");
+
+router.post(
+    "/forgotUsername",
+    bodyParser.json(),
+    authController.forgotUsername
+  );
