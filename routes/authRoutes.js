2fd8b44fa01f8ae9d6526a0cd9f06e35ef4a2172
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-14 00:43:48 +0200

    login route added

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,3 +2,5 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
+
+router.post("/login", bodyParser.json(), userController.login);
