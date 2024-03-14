15628536bdc59037aa808fab2a19e140633d2c3f
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-14 02:11:47 +0200

    route for login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,4 +3,11 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const bodyParser = require("body-parser");
 
-router.post("/login", bodyParser.json(), userController.login);
+router.post(
+    "/login",
+    [
+        check("email", "Please include a valid email").isEmail(),
+        check("password", "Password is required").exists()
+    ],
+    authController.login
+);
