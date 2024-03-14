04a9b6f222adf700a30fba6ab300435370ff3acc
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-14 02:17:31 +0200

    update dependency and secret jwt

--- generateRandomString.js
+++ generateRandomString.js
@@ -1,3 +1,3 @@
 const crypto = require('crypto');
-const randomString = crypto.randomBytes(32).toString('hex');
+const randomString = crypto.randomBytes(64).toString('hex');
 console.log(randomString);
