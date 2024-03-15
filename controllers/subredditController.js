1c0c077dfa8b6a4530dd5526b30f782a1463aa9d
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-03-15 04:19:16 +0200

    sorting

--- controllers/subredditController.js
+++ controllers/subredditController.js
@@ -1,1 +1,35 @@
 const Subreddit = require("../models/subReddit");
+
+const sorting = (req, res, next) => {
+  const subreddit = Subreddit.findById(req.params.id)
+  .then((subreddit) => {
+  const Hot = req.params.Hot;
+  const New = req.params.New; 
+  const Top = req.params.Top;
+  const Random = req.params.Random;
+  if (Hot == true) 
+  {
+    res.json( subreddit.posts.sort((a, b) => b.votes - a.votes) );
+  } else if (New == true) 
+  {
+    res.json( subreddit.posts.sort((a, b) => b.createdAt - a.createdAt) ); // need to add createdAt time stamp
+  } else if (Top == true) 
+  {
+    res.json( subreddit.posts.sort((a, b) => b.comments.length - a.comments.length + b.votes - a.votes) );
+  } else if (Random == true) 
+  {
+    res.json( subreddit.posts.sort(() => Math.random() - 0.5) );
+  }
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+  
+  
+
+};
+
+
+module.exports = {
+    sorting
+};
--- routes/subredditRoutes.js
+++ routes/subredditRoutes.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/subredditController");
 const bodyParser = require("body-parser");
+
+
+router.get(
+    "/sorting",
+    bodyParser.json(),
+    subRedditController.sorting
+);
