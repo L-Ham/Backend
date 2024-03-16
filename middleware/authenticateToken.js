2acb874124369c36943461f1a5717bbea173a0a4
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-16 12:31:58 +0200

    added the file that will authenticate  the token

---
+++ middleware/authenticateToken.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1]; 
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden: Invalid token' });
+    }
+
+    req.userId = decoded.user.id; 
+    next();
+  });
+};
+
+module.exports = authenticateToken;
