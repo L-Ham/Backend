6fad76cc418d5ee0ccf072a960621dce12c9a8ee
Ziad Abd El Wareth <Ziad.Wareth@gmail.com>
2024-03-16 21:19:17 +0200

    Added Google Login Route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,7 +4,7 @@ const authController = require("../controllers/authController");
 const bodyParser = require("body-parser");
 const { check } = require("express-validator");
 router.post("/googleSignUp", bodyParser.json(), authController.googleSignUp);
-router.post("/googleLogin", bodyParser.json(), authController.googleSignUp);
+router.post("/googleLogin", bodyParser.json(), authController.googleLogin);
 router.post("/signUp", bodyParser.json(), authController.signUp);
 router.post(
   "/login",
