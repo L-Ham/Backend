12e5ea171e8f3954e5b3373c3f31f591f46af717
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-16 20:24:40 +0200

    updated login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,85 +1,86 @@
-const User = require("../models/user"); 
-const nodemailer = require('nodemailer'); 
+const User = require("../models/user");
+const nodemailer = require("nodemailer");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
-const authenticateToken = require('../middleware/authenticateToken');
+const authenticateToken = require("../middleware/authenticateToken");
 
 //TODO: add tokens in headers
 const forgetUsername = (req, res, next) => {
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'admin@gmail.com',
-          pass: '123456'
-        }
-      });
-    const email  = req.body.email;
-    const user = User.find(user => user.email === email);
-    if (!user) {
-        return res.status(404).send("User not found");
-    }
-    transporter.sendMail({
-        from: 'admin@gmail.com',
-        to: email,
-        subject: 'So you wanna know your Reddit username, huh?',
-        text: `Hi there,
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "admin@gmail.com",
+      pass: "123456",
+    },
+  });
+  const email = req.body.email;
+  const user = User.find((user) => user.email === email);
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  transporter.sendMail(
+    {
+      from: "admin@gmail.com",
+      to: email,
+      subject: "So you wanna know your Reddit username, huh?",
+      text: `Hi there,
 
         You forgot it didn't you? Hey, it happens. Here you go:
         
         Your username is ${user.username}
         
-        (Username checks out, nicely done.)`
-      }, (err) => {
-        if (err) {
-          console.error('Error sending email:', err);
-          return res.status(500).send('Failed to send email');
-        }
-        res.send('Email sent');
-      });
-
-  };
-
-  
-const login = async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        (Username checks out, nicely done.)`,
+    },
+    (err) => {
+      if (err) {
+        console.error("Error sending email:", err);
+        return res.status(500).send("Failed to send email");
+      }
+      res.send("Email sent");
     }
-
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-            return res.status(400).json({ msg: "Invalid email or password" });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(400).json({ msg: "Invalid email or password" });
-        }
-        const payload = {
-            user: {
-                id: user.id
-            }
-        };
-
-        jwt.sign(
-            payload,
-            process.env.JWT_SECRET,
-            { expiresIn: 3600 }, // Expires in 1 hour 
-            (err, token) => {
-                if (err) throw err;
-                res.json({ token });
-            }
-        );
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server error");
-    }
+  );
 };
 
-module.exports = { login, forgetUsername};
+const login = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ msg: "Invalid email or password" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid email or password" });
+    }
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: 500000000000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token, message: "User logged in successfully" });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+
+module.exports = { login, forgetUsername };
