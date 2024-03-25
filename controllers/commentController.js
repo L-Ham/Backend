264b8f66d05e28e3e37e2675d129285950337c36
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-25 12:25:47 +0200

    added hide and unhide functionalities

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,1 +1,50 @@
 const Comment = require("../models/comment");
+
+const hideComment = (req, res, next) => {
+    const { commentId } = req.params; 
+    Comment.findById(commentId)
+        .then(comment => {
+            if (!comment) {
+                return res.status(404).send("Comment not found");
+            }
+            if (comment.isHidden) {
+                return res.status(200).send("Comment is already hidden");
+            }
+            comment.isHidden = true;
+            return comment.save();
+        })
+        .then(() => {
+            res.status(200).send("Comment hidden successfully");
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400).send("Could not hide comment");
+        });
+};
+
+const unhideComment = (req, res, next) => {
+    const { commentId } = req.params; 
+    Comment.findById(commentId)
+        .then(comment => {
+            if (!comment) {
+                return res.status(404).send("Comment not found");
+            }
+            if (!comment.isHidden) {
+                return res.status(200).send("Comment is already unhidden");
+            }
+            comment.isHidden = false;
+            return comment.save();
+        })
+        .then(() => {
+            res.status(200).send("Comment unhidden successfully");
+        })
+        .catch(err => {
+            console.error(err);
+            res.status(400).send("Could not unhide comment");
+        });
+}
+
+module.exports = {
+    hideComment,
+    unhideComment
+};
