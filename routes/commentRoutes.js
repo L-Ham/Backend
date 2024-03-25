bf10b970209edeb25af034711336245c91d9d572
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-25 12:25:36 +0200

    added hide and unhide routes

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/commentController");
+const commentController = require("../controllers/commentController");
+const authenticateToken = require("../middleware/authenticateToken");
 const bodyParser = require("body-parser");
+
+router.patch(
+    "/hideComment",
+    bodyParser.json(),
+    authenticateToken,
+    commentController.hideComment  
+);
+router.patch(
+    "/unhideComment",
+    bodyParser.json(),
+    authenticateToken,
+    commentController.unhideComment  
+);
+
+module.exports = router;
