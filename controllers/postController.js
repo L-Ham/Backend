2a0b4d03217ae7ac9897f3c0dec43f890b149cca
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-25 15:56:15 +0200

    added create post functionallity

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,95 @@
 const Post = require("../models/post");
 const User = require("../models/user");
+const SubReddit = require("../models/subReddit");
+
+const createPost = (req, res, next) => {
+    const userId = req.userId;
+    const subRedditId = req.body.subreddit;
+    console.log("subRedditId", subRedditId);
+
+    User.findById(userId)
+        .then((user) => {
+            if (!user) {
+                console.error("User not found for user ID:", userId);
+                return res.status(404).json({ message: "User not found" });
+            }
+            if (!subRedditId) {
+                return createPostUnderUser(user);
+            } else {
+                return SubReddit.findById(subRedditId)
+                    .then(subReddit => {
+                        if (!subReddit) {
+                            console.error("Subreddit not found for subreddit ID:", subRedditId);
+                            return createPostUnderUser(user);
+                        } else {
+                            return createPostUnderSubReddit(subReddit);
+                        }
+                    });
+            }
+        })
+        .then(() => {
+            res.status(200).json({ message: "Post created successfully" });
+        })
+        .catch((error) => {
+            console.error("Error creating post:", error);
+            res.status(500).json({ message: "Error creating post" });
+        });
+
+    function createPostUnderUser(user) {
+        const newPost = new Post({
+            user: userId,
+            title: req.body.title,
+            content: req.body.content,
+            type: req.body.type,
+            isNSFW: req.body.isNSFW || false,
+            isSpoiler: req.body.isSpoiler || false,
+            isLocked: req.body.isLocked || false,
+            isOc: req.body.isOc || false,
+            votes: 0,
+            views: 0,
+            commentCount: 0,
+            spamCount: 0,
+            poll: req.body.poll ? {
+                options: req.body.poll.options || [],
+                votingLength: req.body.poll.votingLength || 0,
+                voters: [],
+                startTime: req.body.poll.startTime || null,
+                endTime: req.body.poll.endTime || null,
+            } : {}
+        });
+        user.posts.push(newPost);
+        console.log("user posts", user.posts)
+        return Promise.all([newPost.save(), user.save()]);
+    }
+
+    function createPostUnderSubReddit(subReddit) {
+        const newPost = new Post({
+            user: userId,
+            subReddit: subRedditId,
+            title: req.body.title,
+            content: req.body.content,
+            type: req.body.type,
+            isNSFW: req.body.isNSFW || false,
+            isSpoiler: req.body.isSpoiler || false,
+            isLocked: req.body.isLocked || false,
+            isOc: req.body.isOc || false,
+            votes: 0,
+            views: 0,
+            commentCount: 0,
+            spamCount: 0,
+            poll: req.body.poll ? {
+                options: req.body.poll.options || [],
+                votingLength: req.body.poll.votingLength || 0,
+                voters: [],
+                startTime: req.body.poll.startTime || null,
+                endTime: req.body.poll.endTime || null,
+            } : {}
+        });
+        subReddit.posts.push(newPost);
+        console.log("subreddit posts", subReddit.posts)
+        return Promise.all([newPost.save(), subReddit.save()]);
+    }
+}
 
 const savePost = (req, res, next) => {
     const userId = req.userId;
@@ -31,4 +121,5 @@ const savePost = (req, res, next) => {
 
 module.exports = {
     savePost,
+    createPost
 };
