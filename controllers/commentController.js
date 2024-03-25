eac18ff1174641901a846afd5d46bd455ccf63d2
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-03-26 00:03:58 +0200

    Update commentController.js

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -54,6 +54,8 @@ const createComment = (req, res) => {
                 return res.status(404).json({ message: "User not found" });
             }
             const comment = new Comment({
+                postId: req.body.postId,
+                userId: userId,
                 text: req.body.text,
                 parentCommentId: req.body.parentCommentId,
                 votes: 0,
