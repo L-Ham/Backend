ce7f13fbf841f9a7c0a612b5efa6ba4304e71e7f
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-03-25 12:06:44 +0200

    save post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,9 +1,34 @@
 const Post = require("../models/post");
 const User = require("../models/user");
 
+const savePost = (req, res, next) => {
+    const userId = req.userId;
 
+    User.findById(userId)
+        .then((user) => {
+            if (!user) {
+                console.error("User not found for user ID:", userId);
+                return res.status(404).json({ message: "User not found" });
+            }
 
+            const post = new Post(req.body.Post);
+            user.savedPost.push(post);
+
+            user.save()
+                .then(() => {
+                    res.status(200).json({ message: "Post saved successfully" });
+                })
+                .catch((error) => {
+                    console.error("Error saving post:", error);
+                    res.status(500).json({ message: "Error saving post" });
+                });
+        })
+        .catch((error) => {
+            console.error("Error finding user:", error);
+            res.status(500).json({ message: "Error finding user" });
+        });
+};
 
 module.exports = {
-
-  };
+    savePost,
+};
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/postController");
+const postController = require("../controllers/postController");
 const bodyParser = require("body-parser");
+const authenticateToken = require("../middleware/authenticateToken");
 
-
+router.patch(
+    "/save",
+    bodyParser.json(),
+    authenticateToken,
+    postController.savePost
+  );
 
 module.exports = router;
