eef3e7cd526ad2abfa3ff75a1e362600a7d80f61
Ziad Abd El Wareth <Ziad.Wareth@gmail.com>
2024-03-27 04:23:06 +0200

    Get Account Settings Async Await

--- controllers/userController.js
+++ controllers/userController.js
@@ -98,29 +98,39 @@ const editProfileSettings = (req, res, next) => {
       res.status(500).json({ message: "Server error" });
     });
 };
-const getAccountSettings = (req, res, next) => {
-  const userId = req.userId;
-  if (!userId) {
-    return res.status(404).json({ message: "User not found" });
+// /**
+//  * @param {Object} req - The request object.
+//  * @param {Object} res - The response object.
+//  * @param {Function} next - Retrieves the account settings for a user..
+//  * @returns {Promise<void>} - A promise that resolves when the account settings are retrieved.
+//  */
+const getAccountSettings = async (req, res, next) => {
+  try {
+    const userId = req.userId;
+
+    if (!userId) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const user = await User.findById(userId);
+
+    if (!user) {
+      console.error("User not found for user ID:", userId);
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const accountSettings = {
+      email: user.email,
+      gender: user.gender,
+      connectedToGoogle: user.signupGoogle,
+    };
+
+    console.log("Account settings: ", accountSettings);
+    res.json({ accountSettings });
+  } catch (err) {
+    console.error("Error retrieving account settings:", err);
+    res.status(500).json({ message: "Error Retreiving user", error: err });
   }
-  User.findById(userId)
-    .then((user) => {
-      if (!user) {
-        console.error("User not found for user ID:", userId);
-        return res.status(404).json({ message: "User not found" });
-      }
-      const accountSettings = {
-        email: user.email,
-        gender: user.gender,
-        connectedToGoogle: user.signupGoogle,
-      };
-      console.log("Account settings: ", accountSettings);
-      res.json({ accountSettings });
-    })
-    .catch((err) => {
-      console.error("Error retrieving account settings:", err);
-      res.status(500).json({ message: "Error Retreiving user", error: err });
-    });
 };
 
 const getSafetyAndPrivacySettings = (req, res, next) => {
