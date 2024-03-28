7012b6a27a997c5daae7a6b0bae277b4686c37d2
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-03-28 15:46:51 +0200

    added a check for postId to add uploads to it

--- controllers/userUploadsController.js
+++ controllers/userUploadsController.js
@@ -1,8 +1,12 @@
 const UserUpload = require('../models/userUploads'); 
 const User = require('../models/user');
+const Post = require('../models/post');
 
 async function uploadImage(req, res) {
   const userId = req.userId;
+  const postId = req.body.postId; 
+  console.log('postId from body:', postId);
+
   try {
     const user = await User.findById(userId);
     if (!user) {
@@ -10,23 +14,33 @@ async function uploadImage(req, res) {
       return res.status(404).json({ message: "User not found" });
     }
     
-    // Create a new document in the userUploads collection for the uploaded image
     const newUserUpload = await UserUpload.create({
       userId: user._id,
       filename: req.file.filename,
       originalname: req.file.originalname,
       mimetype: req.file.mimetype,
       url: `/uploads/images/${req.file.filename}`,
+      type: req.body.type,
     });
     console.log('Image uploaded successfully:', newUserUpload);
+    if (req.body.type === 'post') {
+      const post = await Post.findById(postId);
+      if (!post) {
+        console.error("Post not found for post ID:", postId);
+        return res.status(404).json({ message: "Post not found" });
+      }
 
-    // Return success response with the newly created userUpload
+      post.images.push(newUserUpload._id);
+
+      await post.save();
+      console.log('Image added to post successfully:', post);
+    }
+
     res.json({
       message: 'Image uploaded successfully',
       userUpload: newUserUpload
     });
   } catch (error) {
-    // Handle errors
     console.error('Error uploading image:', error);
     res.status(500).json({ error: 'Failed to upload image' });
   }
