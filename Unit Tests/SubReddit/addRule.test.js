ba0e7617ff1dabe818267b3a945f23684b2894e0
Mohamed-Zakaria1 <153124001+Mohamed-Zakaria1@users.noreply.github.com>
2024-04-04 22:02:52 +0200

    Hot fixes

--- Unit Tests/SubReddit/addRule.test.js
+++ Unit Tests/SubReddit/addRule.test.js
@@ -1,111 +1,111 @@
-// const SubReddit = require("../../models/subreddit");
-// const subredditController = require("../../controllers/subredditController");
+const SubReddit = require("../../models/subreddit");
+const subredditController = require("../../controllers/subredditController");
 
-// jest.mock("../../models/subreddit", () => ({
-//   findById: jest.fn(),
-// }));
+jest.mock("../../models/subreddit", () => ({
+  findById: jest.fn(),
+}));
 
-// describe('addRuleWidget', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+describe('addRuleWidget', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//   it('should add a rule widget to the subreddit', async () => {
-//     const subredditId = 'subreddit123';
-//     const ruleWidget = {
-//       rule: 'Some rule',
-//       description: 'Some description',
-//       appliedTo: 'Some appliedTo',
-//       reportReasonDefault: 'Some reportReasonDefault',
-//     };
+  it('should add a rule widget to the subreddit', async () => {
+    const subredditId = 'subreddit123';
+    const ruleWidget = {
+      rule: 'Some rule',
+      description: 'Some description',
+      appliedTo: 'Some appliedTo',
+      reportReasonDefault: 'Some reportReasonDefault',
+    };
 
-//     const subreddit = {
-//       widgets: {
-//         rulesWidgets: [],
-//       },
-//       save: jest.fn().mockResolvedValueOnce({}),
-//     };
+    const subreddit = {
+      widgets: {
+        rulesWidgets: [],
+      },
+      save: jest.fn().mockResolvedValueOnce({}),
+    };
 
-//     SubReddit.findById.mockResolvedValueOnce(subreddit);
+    SubReddit.findById.mockResolvedValueOnce(subreddit);
 
-//     const req = {
-//       body: {
-//         subredditId,
-//         ...ruleWidget,
-//       },
-//     };
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
+    const req = {
+      body: {
+        subredditId,
+        ...ruleWidget,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-//     await subredditController.addRuleWidget(req, res);
+    await subredditController.addRuleWidget(req, res);
 
-//     expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
-//     expect(subreddit.widgets.rulesWidgets).toHaveLength(1);
-//     expect(subreddit.save).toHaveBeenCalled();
-//     expect(res.json).toHaveBeenCalledWith({
-//       message: "Rule added successfully",
-//       savedSubreddit: {},
-//     });
-//   });
+    expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
+    expect(subreddit.widgets.rulesWidgets).toHaveLength(1);
+    expect(subreddit.save).toHaveBeenCalled();
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Rule added successfully",
+      savedSubreddit: {},
+    });
+  });
 
-//   it('should handle error if subreddit is not found', async () => {
-//     const subredditId = 'subreddit123';
-//     const ruleWidget = {
-//       rule: 'Some rule',
-//       description: 'Some description',
-//       appliedTo: 'Some appliedTo',
-//       reportReasonDefault: 'Some reportReasonDefault',
-//     };
+  it('should handle error if subreddit is not found', async () => {
+    const subredditId = 'subreddit123';
+    const ruleWidget = {
+      rule: 'Some rule',
+      description: 'Some description',
+      appliedTo: 'Some appliedTo',
+      reportReasonDefault: 'Some reportReasonDefault',
+    };
 
-//     SubReddit.findById.mockResolvedValueOnce(null);
+    SubReddit.findById.mockResolvedValueOnce(null);
 
-//     const req = {
-//       body: {
-//         subredditId,
-//         ...ruleWidget,
-//       },
-//     };
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
+    const req = {
+      body: {
+        subredditId,
+        ...ruleWidget,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-//     await subredditController.addRuleWidget(req, res);
+    await subredditController.addRuleWidget(req, res);
 
-//     expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
-//     expect(res.status).toHaveBeenCalledWith(404);
-//     expect(res.json).toHaveBeenCalledWith({ message: "Subreddit not found" });
-//   });
+    expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: "Subreddit not found" });
+  });
 
-//   it('should handle server error', async () => {
-//     const subredditId = 'subreddit123';
-//     const ruleWidget = {
-//       rule: 'Some rule',
-//       description: 'Some description',
-//       appliedTo: 'Some appliedTo',
-//       reportReasonDefault: 'Some reportReasonDefault',
-//     };
-//     const errorMessage = 'Some error message';
+  it('should handle server error', async () => {
+    const subredditId = 'subreddit123';
+    const ruleWidget = {
+      rule: 'Some rule',
+      description: 'Some description',
+      appliedTo: 'Some appliedTo',
+      reportReasonDefault: 'Some reportReasonDefault',
+    };
+    const errorMessage = 'Some error message';
 
-//     SubReddit.findById.mockRejectedValueOnce(new Error(errorMessage));
+    SubReddit.findById.mockRejectedValueOnce(new Error(errorMessage));
 
-//     const req = {
-//       body: {
-//         subredditId,
-//         ...ruleWidget,
-//       },
-//     };
-//     const res = {
-//       status: jest.fn().mockReturnThis(),
-//       json: jest.fn(),
-//     };
+    const req = {
+      body: {
+        subredditId,
+        ...ruleWidget,
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
 
-//     await subredditController.addRuleWidget(req, res);
+    await subredditController.addRuleWidget(req, res);
 
-//     expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
-//     expect(res.status).toHaveBeenCalledWith(500);
-//     expect(res.json).toHaveBeenCalledWith({ message: "Error adding rule" });
-//   });
-// });
+    expect(SubReddit.findById).toHaveBeenCalledWith(subredditId);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ message: "Error adding rule" });
+  });
+});
--- Unit Tests/User/deleteSocialLink.test.js
+++ Unit Tests/User/deleteSocialLink.test.js
@@ -1,162 +1,162 @@
-// const User = require("../../models/user");
-// const userController = require("../../controllers/userController");
-// jest.mock("../../models/user");
+const User = require("../../models/user");
+const userController = require("../../controllers/userController");
+jest.mock("../../models/user");
 
-// describe("deleteSocialLink", () => {
-//   it("should delete the social link and return success message", async () => {
-//     const userId = "userId";
-//     const linkId = "linkId";
-//     const user = {
-//       socialLinks: [
-//         {
-//           _id: linkId,
-//           url: "http://example.com",
-//         },
-//       ],
-//       save: jest.fn(),
-//     };
-//     user.socialLinks.pull = function (linkId) {
-//       const linkIndex = this.findIndex(
-//         (link) => link._id.toString() === linkId
-//       );
-//       if (linkIndex > -1) {
-//         this.splice(linkIndex, 1);
-//       }
-//     };
-//     User.findById.mockResolvedValue(user);
+describe("deleteSocialLink", () => {
+  it("should delete the social link and return success message", async () => {
+    const userId = "userId";
+    const linkId = "linkId";
+    const user = {
+      socialLinks: [
+        {
+          _id: linkId,
+          url: "http://example.com",
+        },
+      ],
+      save: jest.fn(),
+    };
+    user.socialLinks.pull = function (linkId) {
+      const linkIndex = this.findIndex(
+        (link) => link._id.toString() === linkId
+      );
+      if (linkIndex > -1) {
+        this.splice(linkIndex, 1);
+      }
+    };
+    User.findById.mockResolvedValue(user);
 
-//     const req = {
-//       params: {
-//         userId,
-//       },
-//       body: {
-//         socialLinkId: linkId,
-//       },
-//     };
-//     const res = {
-//       json: jest.fn(),
-//       status: jest.fn().mockReturnThis(),
-//     };
-//     const next = jest.fn();
-//     // Mock the User.findById method
-//     User.findById = jest.fn().mockImplementation((id) => {
-//       return new Promise((resolve, reject) => {
-//         if (id === userId) {
-//           resolve(user);
-//         } else {
-//           reject("User not found");
-//         }
-//       });
-//     });
+    const req = {
+      params: {
+        userId,
+      },
+      body: {
+        socialLinkId: linkId,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const next = jest.fn();
+    // Mock the User.findById method
+    User.findById = jest.fn().mockImplementation((id) => {
+      return new Promise((resolve, reject) => {
+        if (id === userId) {
+          resolve(user);
+        } else {
+          reject("User not found");
+        }
+      });
+    });
 
-//     // Mock the user.save method
-//     user.save = jest.fn().mockImplementation(() => {
-//       return new Promise((resolve, reject) => {
-//         if (user.socialLinks.length === 0) {
-//           resolve(user);
-//         } else {
-//           reject("Error deleting social link");
-//         }
-//       });
-//     });
-//     await userController.deleteSocialLink(req, res, next);
+    // Mock the user.save method
+    user.save = jest.fn().mockImplementation(() => {
+      return new Promise((resolve, reject) => {
+        if (user.socialLinks.length === 0) {
+          resolve(user);
+        } else {
+          reject("Error deleting social link");
+        }
+      });
+    });
+    await userController.deleteSocialLink(req, res, next);
 
-//     expect(User.findById).toHaveBeenCalledWith(userId);
-//     expect(user.socialLinks.pull).toHaveBeenCalledWith(linkId);
-//     expect(user.save).toHaveBeenCalled();
-//     expect(res.status).toHaveBeenCalledWith(200);
-//     expect(res.json).toHaveBeenCalledWith({
-//       message: "Social link deleted successfully",
-//     });
-//     expect(next).not.toHaveBeenCalled();
-//   });
-//   it("should return 404 if user is not found", async () => {
-//     const userId = "userId";
-//     const linkId = "linkId";
+    expect(User.findById).toHaveBeenCalledWith(userId);
+    expect(user.socialLinks.pull).toHaveBeenCalledWith(linkId);
+    expect(user.save).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Social link deleted successfully",
+    });
+    expect(next).not.toHaveBeenCalled();
+  });
+  it("should return 404 if user is not found", async () => {
+    const userId = "userId";
+    const linkId = "linkId";
 
-//     User.findById = jest.fn().mockResolvedValue(null);
+    User.findById = jest.fn().mockResolvedValue(null);
 
-//     const req = {
-//       userId,
-//       body: {
-//         socialLinkId: linkId,
-//       },
-//     };
-//     const res = {
-//       json: jest.fn(),
-//       status: jest.fn().mockReturnThis(),
-//     };
-//     const next = jest.fn();
+    const req = {
+      userId,
+      body: {
+        socialLinkId: linkId,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const next = jest.fn();
 
-//     await userController.deleteSocialLink(req, res, next);
+    await userController.deleteSocialLink(req, res, next);
 
-//     expect(User.findById).toHaveBeenCalledWith(userId);
-//     expect(res.status).toHaveBeenCalledWith(404);
-//     expect(res.json).toHaveBeenCalledWith({ message: "User not found" });
-//     expect(next).not.toHaveBeenCalled();
-//   });
+    expect(User.findById).toHaveBeenCalledWith(userId);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: "User not found" });
+    expect(next).not.toHaveBeenCalled();
+  });
 
-//   it("should return 404 if social link is not found", async () => {
-//     const userId = "userId";
-//     const linkId = "linkId";
+  it("should return 404 if social link is not found", async () => {
+    const userId = "userId";
+    const linkId = "linkId";
 
-//     const user = {
-//       _id: userId,
-//       socialLinks: [
-//         { _id: "linkId1", url: "https://example.com/link1" },
-//         { _id: "linkId2", url: "https://example.com/link2" },
-//         { _id: "linkId3", url: "https://example.com/link3" },
-//       ],
-//     };
+    const user = {
+      _id: userId,
+      socialLinks: [
+        { _id: "linkId1", url: "https://example.com/link1" },
+        { _id: "linkId2", url: "https://example.com/link2" },
+        { _id: "linkId3", url: "https://example.com/link3" },
+      ],
+    };
 
-//     User.findById = jest.fn().mockResolvedValue(user);
+    User.findById = jest.fn().mockResolvedValue(user);
 
-//     const req = {
-//       userId,
-//       body: {
-//         socialLinkId: linkId,
-//       },
-//     };
-//     const res = {
-//       json: jest.fn(),
-//       status: jest.fn().mockReturnThis(),
-//     };
-//     const next = jest.fn();
+    const req = {
+      userId,
+      body: {
+        socialLinkId: linkId,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const next = jest.fn();
 
-//     await userController.deleteSocialLink(req, res, next);
+    await userController.deleteSocialLink(req, res, next);
 
-//     expect(User.findById).toHaveBeenCalledWith(userId);
-//     expect(res.status).toHaveBeenCalledWith(404);
-//     expect(res.json).toHaveBeenCalledWith({ message: "Social link not found" });
-//     expect(next).not.toHaveBeenCalled();
-//   });
+    expect(User.findById).toHaveBeenCalledWith(userId);
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: "Social link not found" });
+    expect(next).not.toHaveBeenCalled();
+  });
 
-//   it("should handle errors and return 500", async () => {
-//     const userId = "userId";
-//     const linkId = "linkId";
+  it("should handle errors and return 500", async () => {
+    const userId = "userId";
+    const linkId = "linkId";
 
-//     User.findById = jest.fn().mockRejectedValue(new Error("Database error"));
+    User.findById = jest.fn().mockRejectedValue(new Error("Database error"));
 
-//     const req = {
-//       userId,
-//       body: {
-//         socialLinkId: linkId,
-//       },
-//     };
-//     const res = {
-//       json: jest.fn(),
-//       status: jest.fn().mockReturnThis(),
-//     };
-//     const next = jest.fn();
+    const req = {
+      userId,
+      body: {
+        socialLinkId: linkId,
+      },
+    };
+    const res = {
+      json: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    const next = jest.fn();
 
-//     await userController.deleteSocialLink(req, res, next);
+    await userController.deleteSocialLink(req, res, next);
 
-//     expect(User.findById).toHaveBeenCalledWith(userId);
-//     expect(res.status).toHaveBeenCalledWith(500);
-//     expect(res.json).toHaveBeenCalledWith({
-//       message: "Error deleting social link from user",
-//       error: new Error("Database error"),
-//     });
-//     expect(next).not.toHaveBeenCalled();
-//   });
-// });
+    expect(User.findById).toHaveBeenCalledWith(userId);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Error deleting social link from user",
+      error: new Error("Database error"),
+    });
+    expect(next).not.toHaveBeenCalled();
+  });
+});
