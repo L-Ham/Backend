d106621e42a646c5e679cd50a01803249d4ab8e2
Ziad Abd El Wareth <Ziad.Wareth@gmail.com>
2024-04-08 17:53:35 +0200

    feat: add get subreddit Posts

--- controllers/subredditController.js
+++ controllers/subredditController.js
@@ -258,6 +258,40 @@ const reorderRules = async (req, res, next) => {
   }
 };
 
+const getSubredditPosts = async (req, res, next) => {
+  const userId = req.userId;
+  const subredditId = req.body.subredditId;
+  const page = parseInt(req.query.page);
+  const limit = parseInt(req.query.limit);
+  const subreddit = await SubReddit.findById(subredditId);
+  if (!subreddit) {
+    return res.status(404).json({ message: "Subreddit not found" });
+  }
+  
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const results = {};
+
+  if (endIndex < (await model.countDocuments().exec())) {
+    results.next = {
+      page: page + 1,
+      limit: limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    results.previous = {
+      page: page - 1,
+      limit: limit,
+    };
+  }
+  try {
+    results.results = await model.find().limit(limit).skip(startIndex).exec();
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+};
 const editCommunityDetails = async (req, res, next) => {
   const userId = req.userId;
   const subredditId = req.body.subredditId;
@@ -304,4 +338,5 @@ module.exports = {
   deleteTextWidget,
   reorderRules,
   editCommunityDetails,
+  getSubredditPosts,
 };
