4eb8eada1a3491d00a9cc69a562bea2ca5764823
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-04-08 17:18:29 +0200

    Update postController.js

--- controllers/postController.js
+++ controllers/postController.js
@@ -707,6 +707,44 @@ const markAsSpoiler = async (req, res, next) => {
   }
 }
 
+const unmarkAsSpoiler = async (req, res, next) => {
+  const userId = req.userId;
+  const postId = req.body.postId;
+  try {
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
+    }
+    if (!post.isSpoiler) {
+      return res.status(400).json({ message: "Post is not marked as spoiler" });
+    }
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if (post.user.toString() !== userId) {
+      return res
+        .status(401)
+        .json({ message: "User not authorized to unmark post as spoiler" });
+    }
+    if (post.subReddit) {
+      const postSubreddit = await SubReddit.findById(post.subReddit);
+      if (!postSubreddit.moderators.includes(userId)) {
+        return res
+          .status(401)
+          .json({ message: "User not authorized to unmark post as spoiler" });
+      }
+    }
+    post.isSpoiler = false;
+    await post.save();
+    res.status(200).json({ message: "Post unmarked as spoiler" });
+  } catch (error) {
+    console.log("Error Unmarking post as spoiler:", error);
+    res.status(500).json({ message: "Error Unmarking post as spoiler" });
+  }
+}
+
 module.exports = {
   savePost,
   unsavePost,
@@ -726,4 +764,5 @@ module.exports = {
   approvePost,
   removePost,
   markAsSpoiler,
+  unmarkAsSpoiler,
 };
