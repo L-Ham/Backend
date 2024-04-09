6b2f5975c69461441b038b897f3e1e8e44ba4dc3
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-04-10 00:43:01 +0200

    removed: comments

--- models/subReddit.js
+++ models/subReddit.js
@@ -68,51 +68,6 @@ const subRedditSchema = new Schema({
       communityDescription: "",
     },
   },
-  // widgets: {
-  //   type: [Object], // Define it as an array of objects
-  //   default: [], // Default value is an empty array
-  // },
-  // widgets: [
-  //   {
-  //     type: {
-  //       type: String,
-  //       enum: ["textWidgets", "rulesWidgets"],
-  //       required: true,
-  //     },
-  //     data: [
-  //       {
-  //         // For textWidgets
-  //         widgetName: {
-  //           type: String,
-  //           required: false,
-  //         },
-  //         text: {
-  //           type: String,
-  //           required: false,
-  //         },
-  //         // For rulesWidgets
-  //         ruleText: {
-  //           type: String,
-  //           required: false,
-  //           unique: true,
-  //         },
-  //         reportReason: {
-  //           type: String,
-  //           required: false,
-  //         },
-  //         appliesTo: {
-  //           type: String,
-  //           required: false,
-  //         },
-  //         fullDescription: {
-  //           type: String,
-  //           required: false,
-  //         },
-  //       },
-  //     ],
-  //   },
-  // ],
-  //1st VERSION
   textWidgets: [
     {
       widgetName: {
@@ -146,10 +101,7 @@ const subRedditSchema = new Schema({
       },
     },
   ],
-  // widgets: {
-  //   type: Schema.Types.Mixed,
-  //   required: false,
-  // },
+
   appearance: {
     bannerImage: {
       type: Schema.Types.ObjectId,
