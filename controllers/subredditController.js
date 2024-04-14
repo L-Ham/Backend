74b0ecaa1d70e7b475e73a6798987705634cf884
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-04-14 20:07:55 +0200

    fix: get request of getSubreddit Rules

--- controllers/subredditController.js
+++ controllers/subredditController.js
@@ -501,7 +501,8 @@ const editCommunityDetails = async (req, res, next) => {
   }
 };
 const getSubRedditRules = async (req, res, next) => {
-  const subredditId = req.body.subredditId;
+  const subredditId = req.query.subredditId;
+  console.log(subredditId);
   try {
     const subreddit = await SubReddit.findById(subredditId);
     if (!subreddit) {
