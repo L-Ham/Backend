8bf4a5a55421bd508267c2059e6383375fdd926c
Marly Mofeed <109438797+MarlyMofeed@users.noreply.github.com>
2024-05-11 05:33:01 +0300

    fix: edit an error in the model name

--- Unit Tests/SubReddit/editBookMark.test.js
+++ Unit Tests/SubReddit/editBookMark.test.js
@@ -1,5 +1,5 @@
 const User = require("../../models/user");
-const SubReddit = require("../../models/subreddit");
+const SubReddit = require("../../models/subReddit");
 const subredditController = require("../../controllers/subredditController");
 
 jest.mock("../../models/user", () => ({
